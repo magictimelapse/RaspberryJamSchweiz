f4e12f4382a83b3b0896042945ddfb87a6c3f450
Michael Rissi <michael.rissi@gmail.com>
2016-08-02 18:59:37 +0000

    packaged sbb module

---
+++ sbb/sbb/__init__.py
@@ -0,0 +1,1 @@
+from sbb import nextConnection
---
+++ sbb/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='sbb',
+      version='0.0.1',
+      description='Wrapper for transport.opendata.ch',
+      author="Michael Rissi",
+      author_email="michael.rissi@gmail.com",
+      packages=['sbb'],
+      )
