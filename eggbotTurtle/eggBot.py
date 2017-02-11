e8a513082e473450bdac55c9e2d72df0b3ebc35a
Michael Rissi <michael.rissi@gmail.com>
2017-02-11 21:42:35 +0100

    DrawLine implemented

--- eggbotTurtle/eggBot.py
+++ eggbotTurtle/eggBot.py
@@ -1,12 +1,14 @@
 import serial
 import gettext
-
-
+import eggConfig
+import math
 class EggBot(object):
     
 
     def __init__(self):
         self._port = self.__openPort()
+        self.__ServoSetup()
+        self.__penisup = False
 
     def __findPort(self):
         # Find a single EiBotBoard connected to a USB port.
@@ -64,18 +66,18 @@ class EggBot(object):
         return None
 
     def __command(self,cmd):
-    if (self._port is not None) and (cmd is not None):
-        try:
-            self._port.write( cmd )
-            response = self._port.readline()
-            if ( response != 'OK\r\n' ):
-                if ( response != '' ):
-                    print( 'After command ' + cmd + ',' )
-                    print( 'Received bad response from EBB: ' + str( response ) + '.' )
-                else:
-                    print( gettext.gettext( 'EBB Serial Timeout.') )
-        except:
-            pass
+        if (self._port is not None) and (cmd is not None):
+            try:
+                self._port.write( cmd )
+                response = self._port.readline()
+                if ( response != 'OK\r\n' ):
+                    if ( response != '' ):
+                        print( 'After command ' + cmd + ',' )
+                        print( 'Received bad response from EBB: ' + str( response ) + '.' )
+                    else:
+                        print( gettext.gettext( 'EBB Serial Timeout.') )
+            except:
+                pass
 
     def query( self, cmd ):
             # type: (object, object) -> object
@@ -90,6 +92,30 @@ class EggBot(object):
                     return response
             else:
                     return None
+
+    def __ServoSetup( self ):
+        print "hello"
+        # Pen position units range from 0% to 100%, which correspond to
+        # a timing range of 6000 - 30000 in units of 1/(12 MHz).
+        # 1% corresponds to 20 us, or 240 units of 1/(12 MHz).
+
+        intTemp = 240 * ( eggConfig.PENUPPOSTION + 25 )
+        self.__command( 'SC,4,' + str( intTemp ) + '\r' )
+
+        intTemp = 240 * ( eggConfig.PENDOWNPOSITION + 25 )
+        self.__command(  'SC,5,' + str( intTemp ) + '\r'  )
+
+        # Servo speed units are in units of %/second, referring to the
+        # percentages above.  The EBB takes speeds in units of 1/(12 MHz) steps
+        # per 21 ms.  Scaling as above, 1% in 1 second corresponds to
+        # 240 steps/s, which corresponds to 0.240 steps/ms, which corresponds
+        # to 5.04 steps/21 ms.  Rounding this to 5 steps/21 ms is correct
+        # to within 1 %.
+
+        intTemp = 5 * eggConfig.SERVOUPSPEED
+        self.__command(  'SC,11,' + str( intTemp ) + '\r' )
+        intTemp = 5 * eggConfig.SERVODOWNSPEED
+        self.__command(  'SC,12,' + str( intTemp ) + '\r' )
 
 
     def __doTimedPause(self, nPause):
@@ -128,21 +154,50 @@ class EggBot(object):
                     return self.query(  'QB\r' )
 
     def TogglePen( self ):
+            self.__penisup = not self.__penisup
             if (self._port is not None):
                     self.__command(  'TP\r')		
 
     def sendPenUp( self, PenDelay ):
-             """
-
-            :rtype: object
-            """
+            self.__penisup = True
             if (self._port is not None):
                     self.__command(  'SP,1\r')		
                     if (PenDelay > 0):
                             self.__doTimedPause( PenDelay)
 
     def sendPenDown( self, PenDelay ):
+            self.__penisup = False
             if (self._port is not None):
                     self.__command(  'SP,0\r')	
                     if (PenDelay > 0):
                             self.__doTimedPause( PenDelay)
+
+
+
+    def drawLine(self, startX, startY, stopX, stopY):
+        nDeltaX = stopX - startX
+        nDeltaY = stopY - startY
+        if self.__penisup:
+            fspeed = eggConfig.PENUPSPEED
+        else:
+            fspeed = eggConfig.PENDDOWNSPEED
+
+        nTime = int( math.ceil( 1000 / fspeed * math.sqrt(nDeltaX*nDeltaX + nDeltaY*nDeltaY) ) )
+        while ( ( abs( nDeltaX ) > 0 ) or ( abs( nDeltaY ) > 0 ) ):
+				if ( nTime > 750 ):
+					xd = int( round( ( 750.0 * nDeltaX ) / nTime ) )
+					yd = int( round( ( 750.0 * nDeltaY ) / nTime ) )
+					td = int( 750 )
+				else:
+					xd = nDeltaX
+					yd = nDeltaY
+					td = nTime
+					if ( td < 1 ):
+						td = 1		# don't allow zero-time moves.
+				yd2 = -yd
+				xd2 = xd
+				strOutput = ','.join( ['SM', str( td ), str( yd2 ), str( xd2 )] ) + '\r'
+				self.__command( strOutput )
+				nDeltaX -= xd
+				nDeltaY -= yd
+				nTime -= td
--- eggbotTurtle/eggTurtle.py
+++ eggbotTurtle/eggTurtle.py
@@ -4,22 +4,27 @@ import eggConfig
 import eggBot
 
 class EggTurtle(object):
-    def __init__(self):
+    def __init__(self, simulation=True):
         self._turtle = turtle.Turtle()
-        self._eggbot = eggBot.EggBot()
-        self._eggbot.sendEnableMotors(eggConfig.STEP_SCALE)
+        self._simulation = simulation
+        if not self._simulation:
+            self._eggbot = eggBot.EggBot()
+            self._eggbot.sendEnableMotors(eggConfig.STEP_SCALE)
 
     def __del__(self):
-        self._eggbot.sendDisableMotors()
+        if not self._simulation:
+            self._eggbot.sendDisableMotors()
 
     @property
     def pendown(self):
-        self._eggbot.sendPenDown(eggConfig.PENDOWNPOSITION)
+        if not self._simulation:
+            self._eggbot.sendPenDown(eggConfig.LOWERINGPENDELAY)
         return self._turtle.pendown()
 
     @property
     def penup(self):
-        self._eggbot.sendPenUp(eggConfig.PENUPPOSTION)
+        if not self._simulation:
+            self._eggbot.sendPenUp(eggConfig.RAISINGPENDELAY)
         return self._turtle.penup()
 
     def right(self, angle):
@@ -33,11 +38,27 @@ class EggTurtle(object):
         startX, startY = self._turtle.pos()
         self._turtle.forward(distance)
         endX, endY = self._turtle.pos()
-
+        if not self._simulation:
+            self._eggbot.drawLine(startX,startY,endX,endY)
 
     def backward(self, distance):
         startX, startY = self._turtle.pos()
         self._turtle.backward(distance)
         endX, endY = self._turtle.pos()
+        if not self._simulation:
+            self._eggbot.drawLine(startX, startY, endX, endY)
 
+
+def main():
+    et = EggTurtle(simulation=True)
+    rep = 12
+    neck = 6
+    for ii in range(rep):
+        et.right(360/rep)
+        for jj in range(neck):
+            et.right(360/neck)
+            et.forward(24)
+
+if __name__ == "__main__":
+    main()
 
