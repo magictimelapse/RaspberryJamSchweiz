c4b228650f014832e835e7ccb8b8110ff595ffe6
Michael Rissi <michael.rissi@gmail.com>
2017-11-14 22:56:37 +0100

    adjusted servo control with makey makey

--- KuegeliFarbe/testServoWithGetCh.py
+++ KuegeliFarbe/testServoWithGetCh.py
@@ -1,19 +1,26 @@
 from servoControl import auf,zu
 from gibPfeilTaste import gibPfeilTaste
 import time
-states =[False,False,False,False]
-def setServos(states):
-    for i,state in enumerate(states):
-        if state:
-            auf(i)
+servos = {
+    "rechts": {"number": 0, "state":False},
+    "links": {"number": 1, "state":False},
+    "unten": {"number": 2, "state":False},
+    "oben": {"number": 3, "state":False}
+    }
+    
+  
+def setServos(servos):
+    for servo in servos:
+        if servos[servo]["state"]:
+            auf(servos[servo]["number"])
         else:
-            zu(i)
+            zu(servos[servo]["number"])
         time.sleep(0.25)
-setServos(states)
+setServos(servos)
 while True:
     pfeil = gibPfeilTaste()
     print (pfeil)
-    
+    servos[pfeil]["state"] = not servos[pfeil]["state"]
     #states[number] = not states[number]
-    #setServos(states)
+    setServos(servos)
         
